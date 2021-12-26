689757262accb137940187b33998327d7832efdd
8mo10dance <8mo10dance@protonmail.com>
2021-12-26 14:55:17 +0900

    request spec で403になるのを修正する

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -18,5 +18,7 @@ module R01
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << '.example.com'
   end
 end
