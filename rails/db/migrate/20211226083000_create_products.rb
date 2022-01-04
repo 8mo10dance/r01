8330469989396203d29c9c9c4c512025d7eec4f7
tomoya sunda <sunda0707@icloud.com>
2022-01-04 22:08:45 +0900

    products にカラムを追加する
    wip
    
    wip

--- rails/db/migrate/20211226083000_create_products.rb
+++ rails/db/migrate/20211226083000_create_products.rb
@@ -1,8 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
+      t.string :name, null: false, default: ''
+      t.datetime :discarded_at
+      t.references :supplier
 
       t.timestamps
+
+      t.index [:discarded_at]
     end
   end
 end
