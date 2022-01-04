25e8e963ecb679ae789c1e6a1cf095981b40b99d
tomoya sunda <sunda0707@icloud.com>
2022-01-04 22:08:45 +0900

    suppliers にカラムを追加する
    wip

--- rails/db/migrate/20211226080902_create_suppliers.rb
+++ rails/db/migrate/20211226080902_create_suppliers.rb
@@ -1,8 +1,12 @@
 class CreateSuppliers < ActiveRecord::Migration[6.1]
   def change
     create_table :suppliers do |t|
+      t.string :name, null: false, default: ''
+      t.datetime :discarded_at
 
       t.timestamps
+
+      t.index [:discarded_at]
     end
   end
 end
